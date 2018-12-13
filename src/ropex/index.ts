296ad0308e47cabfc12717d3d0310692c50ae484
Isaac Weaver <isaac@couleetechlink.com>
2018-12-13 17:47:41 -0600

    chore: add docstring to ropex function and rename generics

--- src/ropex/index.ts
+++ src/ropex/index.ts
@@ -1,22 +1,30 @@
 import { RopexState, EntryKey } from './types';
 import { RopexStore } from './RopexStore';
 
-export function ropex<T extends object = {}, I extends EntryKey = string>(
-  state: RopexState<T, I>,
-): RopexStore<T, I> {}
+/**
+ * Create a new ropex store
+ *
+ * @param state indexed state
+ *
+ * @template E Store entry type
+ * @template K Entry key type
+ */
+export function ropex<E extends object, K extends EntryKey>(
+  state: RopexState<E, K>,
+): RopexStore<E, K> {}
 
 export namespace ropex {
   /**
    * Create an empty ropex state
    */
-  export function empty<T extends object, I extends EntryKey>(): RopexState<
+  export function empty<T extends object, K extends EntryKey>(): RopexState<
     T,
-    I
+    K
   > {
     return {
-      entries: {} as Record<I, T>,
+      entries: {} as Record<K, T>,
       indexes: {},
-      drafts: {} as Record<I, T>,
+      drafts: {} as Record<K, T>,
     };
   }
 }
